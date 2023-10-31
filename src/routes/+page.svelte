47b7d71578a4ac47dd1ab0cbcc66b60ecf3a20e0
Mark Cruz <mark.cruz@CommerceOne.onmicrosoft.com>
2023-10-31 23:03:50 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    console.log('hello')
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/posts">Posts</a>
+        </li>
+    </ul>
+</nav>
+
+<h1>Home</h1>
